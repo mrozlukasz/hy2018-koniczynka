089d327ed9e8cbb6fb7ca53bbeb78bd19a07f3a5
Michal Zaborowski <michal@zaborowski.info.pl>
2018-11-24 15:34:31 +0100

    ocr - integration with B/W test

--- test/testing-ocr.js
+++ test/testing-ocr.js
@@ -2,13 +2,24 @@ const assert = require("assert");
 const ocr = require("../image-processing/service")
 
 describe("ocr - interface testing", function() {
-    it("sendin color file", function() {
+    it("sending color file", function() {
         //given
-        let file_name = "/home/mzaborow/workspace/hy2018/hy2018-koniczynka/test/img_20181124_101409.jpg";
+        let file_name = "./test/img_20181124_101409.jpg";
         process.env.OCR_TOKEN = 'aabbc6b1b288957';
         //when
         let res = ocr.parseImageFromFile(file_name);
         //then
         console.log(res)
+    });
+
+    it("sendin b/w file", function() {
+        //given
+        let file_name = "./test/46678159_2279068278995327_5677922893883768832_n.jpg1987077106_processed.jpg";
+        process.env.OCR_TOKEN = 'aabbc6b1b288957';
+        //when
+        let res = ocr.parseImageFromFile(file_name);
+        //then
+        console.log("aaa");
+        console.log(res);
     });
 });
