ab4aa78dc862c230705f0318dca2903b399a4340
Łukasz Mróz <l.mroz88@gmail.com>
2018-11-24 23:46:20 +0100

    Obsługa postbacków pt 2.

--- routes/bots/conversation.js
+++ routes/bots/conversation.js
@@ -125,7 +125,7 @@ exports.sendGames = function (request, recipientId, token) {
                         title: "Wytypuj gdzie padnie wygrana",
                         subtitle: "Quiz",
                         item_url: "https://www.lotto.pl/lotto/mapa-kolektur",
-                        image_url: "https://cdn.pixabay.com/photo/2017/02/11/22/38/quiz-2058883_960_720.png",
+                        image_url: "https://hy2018-koniczynka.herokuapp.com/images/gdzie_padnie_wygrana.png",
                         buttons: [{
                             type: "web_url",
                             url: "https://www.quizowa.pl/",
@@ -135,7 +135,7 @@ exports.sendGames = function (request, recipientId, token) {
                         title: "Kup kupon do godziny 18:00",
                         subtitle: "We wskazanej kolekturze",
                         item_url: "https://www.lotto.pl/lotto/mapa-kolektur",
-                        image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/Toicon-icon-hatch-navigate.svg/2000px-Toicon-icon-hatch-navigate.svg.png",
+                        image_url: "https://hy2018-koniczynka.herokuapp.com/images/wskazana_kolektura.png",
                         buttons: [{
                             type: "web_url",
                             url: "https://www.google.pl/maps/dir/52.254699,21.0436229/Przy+Wile%C5%84skiej.+Apteka,+Wile%C5%84ska+9,+03-001+Warszawa/@52.2541613,21.0350818,16z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x471ecc3f414bc595:0x4e9d9d28e8bf3966!2m2!1d21.0357!2d52.256389",
@@ -148,7 +148,7 @@ exports.sendGames = function (request, recipientId, token) {
                             title: "Nie przegap 5 losowań",
                             subtitle: "Weź udział w 5 kolejnych losowaniach",
                             item_url: "https://www.lotto.pl/",
-                            image_url: "https://www.lotto.pl/sites/default/files/svg/lotto_z_bialym_obrysem.svg",
+                            image_url: "https://hy2018-koniczynka.herokuapp.com/images/strike.png",
                             buttons: [{
                                 type: "web_url",
                                 url: "https://www.lotto.pl/",
