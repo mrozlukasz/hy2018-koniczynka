9f9403b59f3811e8fd4b999c83aae0e74453aad0
Łukasz Mróz <l.mroz88@gmail.com>
2018-11-24 15:55:08 +0100

    fix mongo uri

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 var config = {
-    db: process.env.MONGOLAB_URI || "mongodb://localhost/test",
+    db: process.env.MONGODB_URI || "mongodb://localhost/test",
     test_db: "mongodb://localhost/test"
 };
 module.exports = config;
