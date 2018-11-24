29c84eb6c76c6b91b1c66f029ed4bb293ae64a55
Damian Orzepowski <damian.orzepowski@circlekeurope.com>
2018-11-24 14:57:28 +0100

    Download image from url and process, create .tmp directory

--- image-processing/preprocessor.js
+++ image-processing/preprocessor.js
@@ -1,33 +1,49 @@
 const replaceColor = require('replace-color');
 const Jimp = require('jimp');
+const path = require("path");
+const crypto = require("crypto");
+const fs = require('fs');
+const request = require('request');
 
 
+const TMP_DIR=".tmp/";
 
 
-exports.adjust = (imageName) => {
-    let processedImageName = imageName + "_deleted_bck.jpg";
+exports.adjust = (imageUrl) => {
 
-    replaceColor({
-        image: imageName,
-        colors: {
-            type: 'hex',
-            targetColor: '#D3ADA6',
-            replaceColor: '#FFFFFF'
-        },
-        deltaE: 30
-    })
-        .then((jimpObject) =>  {
-            return jimpObject.resize(Jimp.AUTO, 1536);
-        })
-        .then((jimpObject) => {
-            jimpObject.write(processedImageName, (err) => {
-                if(err) return console.log(err)
-            })
-        }).catch((err) => {
-        console.log(err)
+    let urlOfImage = new URL(imageUrl);
+    imageName = TMP_DIR + path.basename(urlOfImage.pathname);
+
+    let downloaded = new Promise((resolve) => {
+        console.log("Donwloading...");
+        request(imageUrl).pipe(fs.createWriteStream(imageName)).on('close', () => resolve(imageName));
     });
 
-    return processedImageName;
+
+    let processedImageName = imageName + crypto.randomBytes(4).readUInt32LE(0) + "_processed.jpg";
+
+    return downloaded.then(() => {
+        console.log("Removing pink...");
+        return replaceColor({
+            image: imageName,
+            colors: {
+                type: 'hex',
+                targetColor: '#D3ADA6',
+                replaceColor: '#FFFFFF'
+            },
+            deltaE: 30
+        }).then((jimpObject) => {
+            console.log("Resizing img...");
+            return jimpObject.resize(Jimp.AUTO, 1536);
+        }).then((jimpObject) => {
+            console.log("Saving processed img", processedImageName , " ...");
+            return jimpObject.writeAsync(processedImageName)
+        }).then(() => {
+            return processedImageName
+        }).catch((err) => {
+            console.log(err)
+        });
+    });
 
 };
 
