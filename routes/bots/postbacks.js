989c2abc019a618d9a0063ee25cc7c14055d81f0
Damian Orzepowski <damian.orzepowski@circlekeurope.com>
2018-11-25 09:44:04 +0100

    fix unknown "_"

--- routes/bots/postbacks.js
+++ routes/bots/postbacks.js
@@ -1,6 +1,7 @@
 const conversations = require('./conversation');
 const games = require("../../game/games");
 const model = require('../../game/state/model');
+const _ = require('lodash');
 
 exports.handle = function (request, event, token) {
     var sender = event.sender.id;
