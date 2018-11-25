7679cbe1ac2801f5b27cacca96405b11430466b5
Michal Zaborowski <michal@zaborowski.info.pl>
2018-11-25 09:38:14 +0100

    calculating winners - console.log

--- routes/win-simulation.js
+++ routes/win-simulation.js
@@ -5,23 +5,28 @@ const model = require('../game/state/model');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    let ctx;
-    let arr;
-    if (_.isEmpty(req.query.win)) {
-        ctx = "Proszę podać parametr win i wartości";
-        arr = "nic nie podano"
-    } else {
-        ctx = "Zwycięzcy są powiadamiani";
-        arr = req.query.win.split(",");
+    try {
+
+        let ctx;
+        let arr;
+        if (_.isEmpty(req.query.win)) {
+            ctx = "Proszę podać parametr win i wartości";
+            arr = "nic nie podano"
+        } else {
+            ctx = "Zwycięzcy są powiadamiani";
+            arr = req.query.win.split(",");
+        }
+
+        model.findWinners(arr);
+
+        res.render('win-simulation', {
+            title: 'Symulowanie losowania....',
+            arr: arr,
+            ctx: ctx
+        });
+    } catch (e) {
+        console.error(e);
     }
-
-    model.findWinners(arr);
-
-    res.render('win-simulation', {
-        title: 'Symulowanie losowania....',
-        arr: arr,
-        ctx: ctx
-    });
 });
 
 module.exports = router;
